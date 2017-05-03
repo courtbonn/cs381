1b3a3378184e36702dfd128fbebff426bc7ef093
henzier <henzier@oregonstate.edu>
2017-05-03 14:29:39 -0700

    Made headway on exercise 2 however....
    Made good progress on exercise 2, however I'm having problems "showing"
    the results... I get an error message, "<interactive>:121:1:
    No instance for (Show D) arising from a use of `print'
    Possible fix: add an instance declaration for (Show D)
    In a stmt of an interactive GHCi command: print it"
    when trying to do sem test1
    
    and the following error (when trying sem2 u)
    "<interactive>:3:1:
    No instance for (Show E) arising from a use of `print'
    Possible fix: add an instance declaration for (Show E)
    In a stmt of an interactive GHCi command: print it"

--- hw2.hs
+++ hw2.hs
@@ -2,14 +2,8 @@ module Hw2 where
 
 --Exercise 1:	A Stack Language
 
+
 type Prog 	= [Cmd]
-type Stack	= [Int]
-type D		= Stack -> Maybe Stack
-
-type State	= (Macros, Stack)
-type Macros	= [(String, Prog)]
-type E		= State -> Maybe State
-
 data Cmd	= LD Int
 		| ADD
 		| MULT
@@ -18,37 +12,69 @@ data Cmd	= LD Int
 		| CALL String
 		deriving Show
 
+type Stack	= Maybe [Int]
+type D		= Stack -> Stack
+
+type Macros	= [(String, Prog)]
+type State	= (Stack, Macros)
+type E		= Maybe State -> Maybe State
+
+
+doExist :: String -> State -> Bool
+doExist s t = s `elem` (map fst (snd t))
+
+findIndex :: String -> State -> Prog
+findIndex s (t,c:cs) 
+				 | fst c == s = snd c
+				 | otherwise  = findIndex s (t,cs)
+
+
 sem :: Prog -> D
-sem []       s 	= Just s
+sem []       s 	= s
 sem (x:xs)   s 	= case (semCmd x s) of
 		  Nothing -> Nothing
-		  Just s' -> sem xs s'
+		  s' -> sem xs s'
 
 semCmd :: Cmd -> D
-semCmd (LD i)       (x) = Just (i:x)
-semCmd (DUP)     (x:xs) = Just (x:x:xs)
+semCmd (LD i)       (Just x) = Just (i:x)
+semCmd (DUP)     (Just (x:xs)) = Just (x:x:xs)
 semCmd (DUP)         _  = Nothing
-semCmd (ADD)   (x:y:xs) = Just ((x+y):xs)
+semCmd (ADD)   (Just (x:y:xs)) = Just ((x+y):xs)
 semCmd (ADD)  	     _	= Nothing
-semCmd (MULT)  (x:y:xs) = Just ((x*y):xs)
+semCmd (MULT)  (Just (x:y:xs)) = Just ((x*y):xs)
 semCmd (MULT)        _  = Nothing
 
 --Exercise 1-2: Extended 
 
 sem2 :: Prog -> E
-sem2 [] s = Just s
-sem2 (x:xs) = case (semCmd2 x s) of
-		  	Nothing -> Nothing
-		    Just s' -> sem2 xs s'
+sem2 [] s = s
+sem2 (x:xs) s = case (semCmd2 x s) of
+		  Nothing -> Nothing
+		  s' -> sem2 xs s'
 
 
 --semCmd2 :: Cmd -> E
 
 
 semCmd2 :: Cmd -> E
-semCmd2 (Cmd c) = 
-semCmd2 (DEF c p) (x, y) = Just ([(c, p)] ++ x, y)
-semCmd2 (CALL c)  (x, y) = (semCmd c x) y
+semCmd2 (LD i) (Just (x,y))	
+				| semCmd (LD i) x == Nothing = Nothing
+				| otherwise		     = Just ((semCmd (LD i) x), y)
+semCmd2 ADD (Just (x,y))
+				| semCmd ADD x == Nothing = Nothing
+				| otherwise		  = Just ((semCmd ADD x), y)
+semCmd2 MULT (Just (x,y))
+				| semCmd MULT x == Nothing = Nothing
+				| otherwise		  = Just ((semCmd MULT x), y)
+semCmd2 DUP (Just (x,y))
+				| semCmd DUP x == Nothing = Nothing
+				| otherwise		  = Just ((semCmd DUP x), y)
+semCmd2 (DEF c p) (Just (x, y)) = if not (doExist c (x,y))
+								   then Just (x,((c,p):y))
+								   else Nothing
+semCmd2 (CALL c)  (Just (x, y)) = if doExist c (x,y)
+								   then sem2 (findIndex c (x,y)) (Just (x,y))
+					 			   else Nothing
 
 --sandbox
 p :: Prog
@@ -64,4 +90,7 @@ test2 :: Prog
 test2 = []
 
 test3 :: Prog
-test3 = [DEF "foo" [DUP, ADD, MULT]]
+test3 = [LD 3, DEF "dadm" [DUP,ADD,DUP,MULT], DEF "dam" [DUP,ADD,DUP], CALL "dadm", CALL "dam"]
+
+u = [LD 3, DEF "dadm" [DUP,ADD,DUP,MULT], CALL "dadm"]
+u' = [LD 3, DEF "dadm" [DUP,ADD,DUP,MULT], DEF "dam" [DUP,ADD,DUP], CALL "dadm", CALL "dam"]
