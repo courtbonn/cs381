324cb4237d77271283c51d2becaac7f65b86f9a4
Courtney Bonn <courtbonn@gmail.com>
2017-05-29 21:32:53 -0700

    finished 1a and 1b

---
+++ hw5.pl
@@ -0,0 +1,35 @@
+
+/* Exercise 1 */
+
+when(275,10).
+when(261,12).
+when(381,11).
+when(398,12).
+when(399,12).
+
+where(275,owen102).
+where(261,dear118).
+where(381,cov216).
+where(398,dear118).
+where(399,cov216).
+
+enroll(mary,275).
+enroll(john,275).
+enroll(mary,261).
+enroll(john,381).
+enroll(jim,399).
+
+/* A */
+schedule(S,P,T) :- enroll(S,Y), where(Y,P), when(Y,T).
+
+/* B */
+usage(P,T) :- where(Y,P), when(Y,T).
+
+/* C */
+
+/* D */
+
+
+/* Exercise 2 */
+
+
